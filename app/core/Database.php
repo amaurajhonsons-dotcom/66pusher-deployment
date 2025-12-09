33134a810861d36345eebae797493872c1aacfe1
Apple <apple@Utkarshs-MacBook-Pro.local>
2025-12-10 02:21:56 +0530

    Refactor DB connection to use explicit port

--- app/core/Database.php
+++ app/core/Database.php
@@ -18,30 +18,33 @@ namespace Altum;
 
 defined('ALTUMCODE') || die();
 
-class Database {
+class Database
+{
 
     public static $database = null;
     public static $db = null;
 
-    public static function initialize() {
+    public static function initialize()
+    {
         mysqli_report(MYSQLI_REPORT_OFF);
 
         self::$database = @new \mysqli(
             DATABASE_SERVER,
             DATABASE_USERNAME,
             DATABASE_PASSWORD,
-            DATABASE_NAME
+            DATABASE_NAME,
+            defined('DATABASE_PORT') ? DATABASE_PORT : 3306
         );
 
         /* Debugging */
-        if(self::$database->connect_error) {
+        if (self::$database->connect_error) {
             include THEME_PATH . 'views/partials/database_error.php';
 
             echo '<div>';
             //echo '<a href="https://altumcode.com/" target="_blank"><img src="https://altumcode.com/themes/altum/assets/images/altumcode.svg" class="altumcode-logo" alt="altumcode logo" loading="lazy" /></a>';
             echo '<h1 class="text-white">Database connection issues</h1>';
             /* Make sure script is actually installed */
-            if(empty(DATABASE_SERVER) && empty(DATABASE_USERNAME) && empty(DATABASE_PASSWORD) && empty(DATABASE_NAME) && !file_exists(ROOT_PATH . 'install/installed')) {
+            if (empty(DATABASE_SERVER) && empty(DATABASE_USERNAME) && empty(DATABASE_PASSWORD) && empty(DATABASE_NAME) && !file_exists(ROOT_PATH . 'install/installed')) {
                 echo '<p>Empty database configuration file (config.php).</p>';
                 echo '<p>Did you run the installer (/install path) of ' . PRODUCT_NAME . '?</p>';
             } else {
@@ -54,7 +57,7 @@ class Database {
         }
 
         /* Mysql profiling */
-        if(MYSQL_DEBUG) {
+        if (MYSQL_DEBUG) {
             self::$database->query("set profiling_history_size=100");
             self::$database->query("set profiling=1");
         }
@@ -66,20 +69,23 @@ class Database {
         return self::$database;
     }
 
-    public static function initialize_helper() {
+    public static function initialize_helper()
+    {
         self::$db = new \Altum\Helpers\MysqliDb(self::$database);
         self::$db->returnType = 'object';
     }
 
-    public static function close() {
+    public static function close()
+    {
 
-        if(!self::$database) return;
+        if (!self::$database)
+            return;
 
-        if(MYSQL_DEBUG) {
+        if (MYSQL_DEBUG) {
             $result = self::$database->query("show profiles");
 
-            while($profile = $result->fetch_object()) {
-                echo $profile->Query_ID . ' - ' . round($profile->Duration, 10)  . ' s - ' . $profile->Query . '<br />';
+            while ($profile = $result->fetch_object()) {
+                echo $profile->Query_ID . ' - ' . round($profile->Duration, 10) . ' s - ' . $profile->Query . '<br />';
             }
         }
 
--- config.php
+++ config.php
@@ -4,11 +4,12 @@
 $db_url = getenv('DATABASE_URL');
 $db_opts = $db_url ? parse_url($db_url) : [];
 
-define('DATABASE_SERVER',   isset($db_opts['host']) ? $db_opts['host'] . (isset($db_opts['port']) ? ':' . $db_opts['port'] : '') : 'localhost');
+define('DATABASE_SERVER', isset($db_opts['host']) ? $db_opts['host'] : 'localhost');
 define('DATABASE_USERNAME', isset($db_opts['user']) ? $db_opts['user'] : 'root');
 define('DATABASE_PASSWORD', isset($db_opts['pass']) ? $db_opts['pass'] : '');
-define('DATABASE_NAME',     isset($db_opts['path']) ? ltrim($db_opts['path'], '/') : '66pusher');
-define('SITE_URL',          getenv('APP_URL') ?: 'http://localhost');
+define('DATABASE_NAME', isset($db_opts['path']) ? ltrim($db_opts['path'], '/') : '66pusher');
+define('DATABASE_PORT', isset($db_opts['port']) ? $db_opts['port'] : 3306);
+define('SITE_URL', getenv('APP_URL') ?: 'http://localhost');
 
 /* Only modify this if you want to use redis for caching instead of the default file system caching */
 define('REDIS_IS_ENABLED', 0);
