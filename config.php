7578ef41940e5271018d7a05139bb15ca9a8b5d2
Apple <apple@Utkarshs-MacBook-Pro.local>
2025-12-10 02:11:52 +0530

    Configure config.php to read DATABASE_URL from env

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   '');
-define('DATABASE_USERNAME', '');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME',     '');
-define('SITE_URL',          '');
+$db_url = getenv('DATABASE_URL');
+$db_opts = $db_url ? parse_url($db_url) : [];
+
+define('DATABASE_SERVER',   isset($db_opts['host']) ? $db_opts['host'] . (isset($db_opts['port']) ? ':' . $db_opts['port'] : '') : 'localhost');
+define('DATABASE_USERNAME', isset($db_opts['user']) ? $db_opts['user'] : 'root');
+define('DATABASE_PASSWORD', isset($db_opts['pass']) ? $db_opts['pass'] : '');
+define('DATABASE_NAME',     isset($db_opts['path']) ? ltrim($db_opts['path'], '/') : '66pusher');
+define('SITE_URL',          getenv('APP_URL') ?: 'http://localhost');
 
 /* Only modify this if you want to use redis for caching instead of the default file system caching */
 define('REDIS_IS_ENABLED', 0);
